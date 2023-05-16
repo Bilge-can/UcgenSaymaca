e019fb168e9ee4047b455b1436fcd649a37b8581
Bilge-can <bilgecan618@gmail.com>
2023-05-16 19:00:29 +0300

    menuSahneKodlama

--- Assets/Scripts/menuLevel/menuManager.cs
+++ Assets/Scripts/menuLevel/menuManager.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class menuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject startBtn, exitBtn;
+
     void Start()
     {
-        
+        FadeOut();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FadeOut()
     {
-        
+        startBtn.GetComponent<CanvasGroup>().DOFade(1, 0.8f);
+        exitBtn.GetComponent<CanvasGroup>().DOFade(1, 0.8f).SetDelay(0.5f);
+
     }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
 }
